8bdde8a391edb758e99998747b210ee6ac3334ba
Martin Schröder <mkschreder.uk@gmail.com>
2017-01-02 14:48:15 +0100

    optimize stack usage in freertos config

--- src/main/FreeRTOSConfig.h
+++ src/main/FreeRTOSConfig.h
@@ -67,6 +67,8 @@
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
+//#pragma GCC poison malloc free
+
 /*-----------------------------------------------------------
  * Application specific definitions.
  *
@@ -85,17 +87,27 @@
 #define configCPU_CLOCK_HZ				( ( unsigned long ) 72000000 )
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 5 )
-#define configMINIMAL_STACK_SIZE		( ( unsigned short ) 512 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 6 * 1024 ) )
+#define configMINIMAL_STACK_SIZE		( ( unsigned short ) 64 )
+
+#ifdef SITL
+	// posix port does not even use stack for threads
+	#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 100 * 1024 ) )
+	#include <stdio.h>
+	#define traceMALLOC(ret, siz) printf("MALLOC: %p %lu\n", ret, siz)
+#else
+	#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 8 * 1024 ) )
+#endif
+
 #define configMAX_TASK_NAME_LEN			( 10 )
 #define configUSE_TRACE_FACILITY		1
 #define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			0
 #define configUSE_MUTEXES				1
 #define configQUEUE_REGISTRY_SIZE		0
-#define configCHECK_FOR_STACK_OVERFLOW	2
+#define configCHECK_FOR_STACK_OVERFLOW	1
 #define configUSE_RECURSIVE_MUTEXES		1
 #define configUSE_MALLOC_FAILED_HOOK	1
+#define configAPPLICATION_ALLOCATED_HEAP 0
 //#define configUSE_APPLICATION_TASK_TAG	0
 #define configUSE_COUNTING_SEMAPHORES	0
 #define configGENERATE_RUN_TIME_STATS	0		//TT - TODO
@@ -108,8 +120,8 @@
 /* Software timer definitions. */
 #define configUSE_TIMERS				1
 #define configTIMER_TASK_PRIORITY		( 2 )
-#define configTIMER_QUEUE_LENGTH		10
-#define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE * 2 )
+#define configTIMER_QUEUE_LENGTH		3
+#define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE )
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
